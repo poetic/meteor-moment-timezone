e4495dbb16f0528c8fa69c0c33ae29d290115eb3
Leonidez Acosta <leonidez@poeticsystems.com>
2015-06-26 17:41:18 -0500

    define package and add README

---
+++ README.md
@@ -0,0 +1,5 @@
+# meteor-moment-timezone
+
+This meteor package installs moment @ v2.10.3 and moment-timezone @ 0.4.0.
+
+For usage please see [the official documentation](http://momentjs.com/timezone/)
--- package.js
+++ package.js
@@ -1,18 +1,20 @@
 Package.describe({
   name: 'poetic:moment-timezone',
-  version: '0.0.1',
+  version: '0.4.0',
   summary: 'current version of Moment Timezone (v0.4.0)',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
+  git: 'https://github.com/poetic/meteor-moment-timezone',
   documentation: 'README.md'
 });
 
-Npm.depends({'moment-timezone': '0.4.0'});
-
 Package.onUse(function(api) {
+  var where = ['client', 'server'];
+
   api.versionsFrom('1.1.0.2');
 
-  api.addFiles('moment-timezone.js');
+  api.use('momentjs:moment@2.10.3', where);
+  api.imply('momentjs:moment@2.10.3', where);
+
+  api.addFiles('pre.js', where);
+  api.addFiles('moment-timezone.js', where);
+  api.addFiles('post.js', where);
 });
